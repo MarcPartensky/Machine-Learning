bd7ba33ae3b806d188a6c07a02f87af0b8bbe924
Marc Partensky <marc.partensky@gmail.com>
2020-10-30 19:28:49 +0100

    checking cuda

--- learning-pytorch/tensor_tutorial.py
+++ learning-pytorch/tensor_tutorial.py
@@ -40,9 +40,11 @@ import torch
 # print(a)
 # print(b)
 
-import numpy as np
-a = np.ones(5)
-b = torch.from_numpy(a)
-np.add(a, 1, out=a)
-print(a)
-print(b)
+# import numpy as np
+# a = np.ones(5)
+# b = torch.from_numpy(a)
+# np.add(a, 1, out=a)
+# print(a)
+# print(b)
+
+print(torch.cuda.is_available())
